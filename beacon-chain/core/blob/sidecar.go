00eb2cb0d9bbf1dba24f25095344d848aca473d9
Roberto Bayardo <bayardo@alum.mit.edu>
2022-11-13 18:18:13 -0800

    have CL compute the aggregated proof for the block, per updated spec

--- beacon-chain/core/blob/sidecar.go
+++ beacon-chain/core/blob/sidecar.go
@@ -5,6 +5,7 @@ import (
 	"github.com/prysmaticlabs/prysm/v3/beacon-chain/core/blocks"
 	"github.com/prysmaticlabs/prysm/v3/consensus-types/interfaces"
 	types "github.com/prysmaticlabs/prysm/v3/consensus-types/primitives"
+	"github.com/prysmaticlabs/prysm/v3/encoding/bytesutil"
 	v1 "github.com/prysmaticlabs/prysm/v3/proto/engine/v1"
 	eth "github.com/prysmaticlabs/prysm/v3/proto/prysm/v1alpha1"
 )
@@ -21,36 +22,36 @@ func (s commitmentSequenceImpl) Len() int {
 	return len(s)
 }
 
-type blobImpl [][]byte
+type BlobImpl [][]byte
 
-func (b blobImpl) At(i int) [32]byte {
+func (b BlobImpl) At(i int) [32]byte {
 	var out [32]byte
-	copy(out[:], b[i][:])
+	copy(out[:], b[i])
 	return out
 }
 
-func (b blobImpl) Len() int {
+func (b BlobImpl) Len() int {
 	return len(b)
 }
 
-type blobsSequenceImpl []*v1.Blob
+type BlobsSequenceImpl []*v1.Blob
 
-func (s blobsSequenceImpl) At(i int) kzg.Blob {
-	return blobImpl(s[i].Blob)
+func (s BlobsSequenceImpl) At(i int) kzg.Blob {
+	return BlobImpl(s[i].Blob)
 }
 
-func (s blobsSequenceImpl) Len() int {
+func (s BlobsSequenceImpl) Len() int {
 	return len(s)
 }
 
 // ValidateBlobsSidecar verifies the integrity of a sidecar, returning nil if the blob is valid.
 func ValidateBlobsSidecar(slot types.Slot, root [32]byte, commitments [][]byte, sidecar *eth.BlobsSidecar) error {
 	kzgSidecar := kzg.BlobsSidecar{
-		BeaconBlockSlot: kzg.Slot(sidecar.BeaconBlockSlot),
-		Blobs:           blobsSequenceImpl(sidecar.Blobs),
+		BeaconBlockRoot:    kzg.Root(bytesutil.ToBytes32(sidecar.BeaconBlockRoot)),
+		BeaconBlockSlot:    kzg.Slot(sidecar.BeaconBlockSlot),
+		Blobs:              BlobsSequenceImpl(sidecar.Blobs),
+		KZGAggregatedProof: kzg.KZGProof(bytesutil.ToBytes48(sidecar.AggregatedProof)),
 	}
-	copy(kzgSidecar.BeaconBlockRoot[:], sidecar.BeaconBlockRoot)
-	copy(kzgSidecar.KZGAggregatedProof[:], sidecar.AggregatedProof)
 	return kzg.ValidateBlobsSidecar(kzg.Slot(slot), kzg.Root(root), commitmentSequenceImpl(commitments), kzgSidecar)
 }
 
